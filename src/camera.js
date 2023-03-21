d3131b9dba124fdecf2308041995b101b8b2bf09
ericjwittrock <ericjwittrock@gmail.com>
2023-03-21 11:01:42 -0500

    fixed projection math and focal length

--- src/camera.js
+++ src/camera.js
@@ -1,8 +1,7 @@
 class Camera3D {
     constructor() {
-        this.pos = new Vector(-10,0,0); // apeture position
-        this.norm = new Vector(1,0,0);
-        this.focalLength = 0.5;
+        this.pos = new Vector(0,0,-4); // apeture position
+        this.focalLength = 4;
     }
     projectPoint(v) {
         let dv = v.sub(this.pos); // vector from apeture to point
@@ -10,7 +9,7 @@ class Camera3D {
         // TODO get focal length to work
         //let sensorHit = Vector.planeIntersect(this.norm, this.pos.add(this.norm.scale(this.focalLength)), dv.norm());
 
-        let sensorHit = Vector.intersectDimension(3, v, dv.norm().scale(-1))
+        let sensorHit = Vector.intersectDimension(3, dv.add(this.pos.scale(-this.focalLength)), dv.norm().scale(-1))
 
         // use x coord for depth
         //sensorHit.x = dv.mag();
